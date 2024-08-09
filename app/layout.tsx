4bf66d200dff84082c60d700605f422013902458
thiagoifrn <thiagoifrn@gmail.com>
2024-08-08 22:28:21 -0300

    chore: adjust conf tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+//import Footer from "./_components/footer"
+//import AuthProvider from "./_providers/auth"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        {/* <AuthProvider> */}
+        <div className="flex h-full flex-col">
+          <div className="flex-1">{children}</div>
+          {/* <Footer /> */}
+        </div>
+        {/* </AuthProvider> */}
+        <Toaster />
+      </body>
     </html>
   );
 }
