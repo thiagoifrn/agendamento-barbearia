f5c82fa1839eb0127c98b65de10c66ac3194968e
thiagoifrn <thiagoifrn@gmail.com>
2024-08-09 00:21:45 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Teste</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h1 className="text-xl font-bold">Olá, Thiago</h1>
+        <p>segunda-feira, 9 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
